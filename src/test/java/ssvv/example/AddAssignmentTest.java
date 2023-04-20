72eb6935a465e6903862efd6405cd7ceca1f1571
Olivia Burca <72073746+912-burca-olivia@users.noreply.github.com>
2023-04-20 12:29:40 +0300

    added rest of tests for addAssignment

--- src/test/java/ssvv/example/AddAssignmentTest.java
+++ src/test/java/ssvv/example/AddAssignmentTest.java
@@ -13,7 +13,7 @@ import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertTrue;
 public class AddAssignmentTest {
     @Test
-    public void testAddAssignmentValidDeadline() {
+    public void testAddAssignmentValid() {
         Validator<Student> studentValidator = new StudentValidator();
         Validator<Tema> temaValidator = new TemaValidator();
         Validator<Nota> notaValidator = new NotaValidator();
@@ -32,6 +32,9 @@ public class AddAssignmentTest {
             }
         }
         assertEquals(addedAssignment.getDeadline(), 13);
+        assertEquals(addedAssignment.getID(), "10");
+        assertEquals(addedAssignment.getDescriere(), "Tema");
+        assertEquals(addedAssignment.getStartline(), 2);
     }
 
     @Test
@@ -49,5 +52,126 @@ public class AddAssignmentTest {
             thrown = true;
         }
         assertTrue(thrown);
+
+        assignment.setDeadline(0);
+        thrown = false;
+        try {
+            Tema result = assignmentFileRepo.save(assignment);
+            assertEquals(result, null);
+            assignmentValidator.validate(assignment);
+        } catch (ValidationException e) {
+            thrown = true;
+        }
+        assertTrue(thrown);
+
+        assignment.setDeadline(1);
+        thrown = false;
+        try {
+            Tema result = assignmentFileRepo.save(assignment);
+            assertEquals(result, null);
+            assignmentValidator.validate(assignment);
+        } catch (ValidationException e) {
+            thrown = true;
+        }
+        assertTrue(thrown);
+
+    }
+
+    @Test
+    public void testAddAssignmentInvalidStartline() {
+        Tema assignment = new Tema("11", "Tema", 10, -1);
+        boolean thrown = false;
+        Validator<Tema> assignmentValidator = new TemaValidator();
+        TemaXMLRepository assignmentFileRepo = new TemaXMLRepository(assignmentValidator, "teme.xml");
+
+        try {
+            Tema result = assignmentFileRepo.save(assignment);
+            assertEquals(result, null);
+            assignmentValidator.validate(assignment);
+        } catch (ValidationException e) {
+            thrown = true;
+        }
+        assertTrue(thrown);
+
+        assignment.setStartline(11);
+        thrown = false;
+        try {
+            Tema result = assignmentFileRepo.save(assignment);
+            assertEquals(result, null);
+            assignmentValidator.validate(assignment);
+        } catch (ValidationException e) {
+            thrown = true;
+        }
+        assertTrue(thrown);
+
+        assignment.setStartline(16);
+        thrown = false;
+        try {
+            Tema result = assignmentFileRepo.save(assignment);
+            assertEquals(result, null);
+            assignmentValidator.validate(assignment);
+        } catch (ValidationException e) {
+            thrown = true;
+        }
+        assertTrue(thrown);
+
+    }
+
+    @Test
+    public void testAddAssignmentInvalidID() {
+        Tema assignment = new Tema(null, "Tema", 10, 8);
+        boolean thrown = false;
+        Validator<Tema> assignmentValidator = new TemaValidator();
+        TemaXMLRepository assignmentFileRepo = new TemaXMLRepository(assignmentValidator, "teme.xml");
+
+        try {
+            Tema result = assignmentFileRepo.save(assignment);
+            assertEquals(result, null);
+            assignmentValidator.validate(assignment);
+        } catch (ValidationException e) {
+            thrown = true;
+        }
+        assertTrue(thrown);
+
+        assignment.setID("");
+        thrown = false;
+        try {
+            Tema result = assignmentFileRepo.save(assignment);
+            assertEquals(result, null);
+            assignmentValidator.validate(assignment);
+        } catch (ValidationException e) {
+            thrown = true;
+        }
+        assertTrue(thrown);
+
+    }
+
+    @Test
+    public void testAddAssignmentInvalidDescription() {
+        Tema assignment = new Tema("10", null, 10, 8);
+        boolean thrown = false;
+        Validator<Tema> assignmentValidator = new TemaValidator();
+        TemaXMLRepository assignmentFileRepo = new TemaXMLRepository(assignmentValidator, "teme.xml");
+
+        try {
+            Tema result = assignmentFileRepo.save(assignment);
+            assertEquals(result, null);
+            assignmentValidator.validate(assignment);
+        } catch (ValidationException e) {
+            thrown = true;
+        }
+        assertTrue(thrown);
+
+        assignment.setDescriere("");
+        thrown = false;
+        try {
+            Tema result = assignmentFileRepo.save(assignment);
+            assertEquals(result, null);
+            assignmentValidator.validate(assignment);
+        } catch (ValidationException e) {
+            thrown = true;
+        }
+        assertTrue(thrown);
+
     }
 }
