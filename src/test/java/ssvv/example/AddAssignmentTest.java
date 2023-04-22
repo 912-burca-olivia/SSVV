9de9d56cd05f514a2708332f49668f6518d370e4
Olivia Burca <72073746+912-burca-olivia@users.noreply.github.com>
2023-04-22 14:27:31 +0300

    simplified tests

--- src/test/java/ssvv/example/AddAssignmentTest.java
+++ src/test/java/ssvv/example/AddAssignmentTest.java
@@ -1,4 +1,5 @@
 package ssvv.example;
+import org.junit.Before;
 import org.junit.Test;
 import ssvv.example.domain.Nota;
 import ssvv.example.domain.Student;
@@ -12,18 +13,29 @@ import ssvv.example.validation.*;
 import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertTrue;
 public class AddAssignmentTest {
-    @Test
-    public void testAddAssignmentValid() {
+    private StudentXMLRepository studentRepo;
+    private TemaXMLRepository assignmentRepo;
+    private NotaXMLRepository gradeRepo;
+    private Service service;
+
+    @Before
+    public void setUp() {
         Validator<Student> studentValidator = new StudentValidator();
         Validator<Tema> temaValidator = new TemaValidator();
         Validator<Nota> notaValidator = new NotaValidator();
 
-        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
-        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
-        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+        studentRepo = new StudentXMLRepository(studentValidator, "studenti.xml");
+        assignmentRepo = new TemaXMLRepository(temaValidator, "teme.xml");
+        gradeRepo = new NotaXMLRepository(notaValidator, "note.xml");
 
-        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
-        service.saveTema("10", "Tema", 13, 2);
+        service = new Service(studentRepo, assignmentRepo, gradeRepo);
+    }
+
+
+    @Test
+    public void testAddAssignmentValid() {
+        int result = service.saveTema("10", "Tema", 13, 2);
+        assertEquals(result , 0);
         Tema addedAssignment = null;
         for (Tema assignment : service.findAllTeme()) {
             if (assignment.getID().equals("10")) {
@@ -39,139 +51,27 @@ public class AddAssignmentTest {
 
     @Test
     public void testAddAssignmentInvalidDeadline() {
-        Tema assignment = new Tema("11", "Tema", 15, 2);
-        boolean thrown = false;
-        Validator<Tema> assignmentValidator = new TemaValidator();
-        TemaXMLRepository assignmentFileRepo = new TemaXMLRepository(assignmentValidator, "teme.xml");
-
-        try {
-            Tema result = assignmentFileRepo.save(assignment);
-            assertEquals(result, null);
-            assignmentValidator.validate(assignment);
-        } catch (ValidationException e) {
-            thrown = true;
-        }
-        assertTrue(thrown);
-
-        assignment.setDeadline(0);
-        thrown = false;
-        try {
-            Tema result = assignmentFileRepo.save(assignment);
-            assertEquals(result, null);
-            assignmentValidator.validate(assignment);
-        } catch (ValidationException e) {
-            thrown = true;
-        }
-        assertTrue(thrown);
-
-        assignment.setDeadline(1);
-        thrown = false;
-        try {
-            Tema result = assignmentFileRepo.save(assignment);
-            assertEquals(result, null);
-            assignmentValidator.validate(assignment);
-        } catch (ValidationException e) {
-            thrown = true;
-        }
-        assertTrue(thrown);
+        int result = service.saveTema("11", "Tema", 15, 2);
+        assertEquals(result, 1);
 
     }
 
     @Test
     public void testAddAssignmentInvalidStartline() {
-        Tema assignment = new Tema("11", "Tema", 10, -1);
-        boolean thrown = false;
-        Validator<Tema> assignmentValidator = new TemaValidator();
-        TemaXMLRepository assignmentFileRepo = new TemaXMLRepository(assignmentValidator, "teme.xml");
-
-        try {
-            Tema result = assignmentFileRepo.save(assignment);
-            assertEquals(result, null);
-            assignmentValidator.validate(assignment);
-        } catch (ValidationException e) {
-            thrown = true;
-        }
-        assertTrue(thrown);
-
-        assignment.setStartline(11);
-        thrown = false;
-        try {
-            Tema result = assignmentFileRepo.save(assignment);
-            assertEquals(result, null);
-            assignmentValidator.validate(assignment);
-        } catch (ValidationException e) {
-            thrown = true;
-        }
-        assertTrue(thrown);
-
-        assignment.setStartline(16);
-        thrown = false;
-        try {
-            Tema result = assignmentFileRepo.save(assignment);
-            assertEquals(result, null);
-            assignmentValidator.validate(assignment);
-        } catch (ValidationException e) {
-            thrown = true;
-        }
-        assertTrue(thrown);
+        int result = service.saveTema("11", "Tema", 10, -2);
+        assertEquals(result, 1);
 
     }
 
     @Test
     public void testAddAssignmentInvalidID() {
-        Tema assignment = new Tema(null, "Tema", 10, 8);
-        boolean thrown = false;
-        Validator<Tema> assignmentValidator = new TemaValidator();
-        TemaXMLRepository assignmentFileRepo = new TemaXMLRepository(assignmentValidator, "teme.xml");
-
-        try {
-            Tema result = assignmentFileRepo.save(assignment);
-            assertEquals(result, null);
-            assignmentValidator.validate(assignment);
-        } catch (ValidationException e) {
-            thrown = true;
-        }
-        assertTrue(thrown);
-
-        assignment.setID("");
-        thrown = false;
-        try {
-            Tema result = assignmentFileRepo.save(assignment);
-            assertEquals(result, null);
-            assignmentValidator.validate(assignment);
-        } catch (ValidationException e) {
-            thrown = true;
-        }
-        assertTrue(thrown);
-
+        int result = service.saveTema(null, "Tema", 10, 8);
+        assertEquals(result, 1);
     }
 
     @Test
     public void testAddAssignmentInvalidDescription() {
-        Tema assignment = new Tema("10", null, 10, 8);
-        boolean thrown = false;
-        Validator<Tema> assignmentValidator = new TemaValidator();
-        TemaXMLRepository assignmentFileRepo = new TemaXMLRepository(assignmentValidator, "teme.xml");
-
-        try {
-            Tema result = assignmentFileRepo.save(assignment);
-            assertEquals(result, null);
-            assignmentValidator.validate(assignment);
-        } catch (ValidationException e) {
-            thrown = true;
-        }
-        assertTrue(thrown);
-
-        assignment.setDescriere("");
-        thrown = false;
-        try {
-            Tema result = assignmentFileRepo.save(assignment);
-            assertEquals(result, null);
-            assignmentValidator.validate(assignment);
-        } catch (ValidationException e) {
-            thrown = true;
-        }
-        assertTrue(thrown);
-
+        int result = service.saveTema("10", "", 10, 8);
+        assertEquals(result, 1);
     }
 }
